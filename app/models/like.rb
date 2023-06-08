2781264a17a5e92e4b7a09187a1f783145ab7fab
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-06-08 09:49:00 +0300

    Fix bugs and Linters

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,5 @@
 class Like < ApplicationRecord
-  belongs_to :author, foreign_key: :author_id
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post
 
   def udate_counter_likes
