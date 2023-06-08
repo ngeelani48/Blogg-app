d24466e0357c1992468e0a3bf685e3737d887cfa
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-06-08 11:52:55 +0300

    Change the likes counter for a post instead of author

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,15 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post
 
+  after_save :udate_counter_likes
+
   def udate_counter_likes
-    author.update(likes_counter: author.likes.count)
+    post.update(likes_counter: post.likes.count)
+  end
+
+  private
+
+  def update_counter_likes
+    post.update(likes_counter: post.likes.count)
   end
 end
