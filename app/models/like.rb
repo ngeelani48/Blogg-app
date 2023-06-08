1a4ba5ff6b5466f065b7085f9fb0f6bec31d87ce
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-06-08 08:21:24 +0300

    Fix Linters

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,8 @@
 class Like < ApplicationRecord
-    belongs_to :author, foreign_key: :author_id
-    belongs_to :post
-  
-    def udate_counter_likes
-      author.update(likes_counter: author.likes.count)
-    end
+  belongs_to :author, foreign_key: :author_id
+  belongs_to :post
+
+  def udate_counter_likes
+    author.update(likes_counter: author.likes.count)
   end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,9 @@
 class Post < ApplicationRecord
-    belongs_to :author, foreign_key: :author_id
-    has_many :comments
-    has_many :likes
-  
-    def update_counter_posts
-        author.update(posts_counter: author.posts.count)
-    end
+  belongs_to :author, foreign_key: :author_id
+  has_many :comments
+  has_many :likes
+
+  def update_counter_posts
+    author.update(posts_counter: author.posts.count)
+  end
+end
