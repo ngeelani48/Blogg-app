b69d9f410edf25bb7f857330f950eea73e438255
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-06-21 17:49:04 +0300

    Generated devise views for confirmations

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users # Add this line to include Devise routes
+
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:create]
       resources :likes, only: [:create]
     end
   end
-  
+
   root "users#index" # Set the root path to the users index action
 
   # Add more routes as needed
