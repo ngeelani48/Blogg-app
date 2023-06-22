6b1e5efd739faab464d0e0c1c56f65abefa2836d
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-06-22 16:55:36 +0300

    Added a role column to the users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_21_184530) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_22_134849) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,6 +58,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_21_184530) do
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
     t.datetime "remember_created_at"
+    t.string "role"
   end
 
   add_foreign_key "comments", "posts"
